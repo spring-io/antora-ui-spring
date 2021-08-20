12613023144ec6f43a0c3d5defdfebd764d85fe8
Rob Winch <rwinch@users.noreply.github.com>
2021-08-20 11:35:34 -0500

    Always preserve css variables
    This is necessary for the themes to work

--- gulp.d/tasks/build.js
+++ gulp.d/tasks/build.js
@@ -48,7 +48,7 @@ module.exports = (src, dest, preview) => () => {
         },
       },
     ]),
-    postcssVar({ preserve: preview }),
+    postcssVar({ preserve: true }),
     preview ? postcssCalc : () => {},
     autoprefixer,
     preview
