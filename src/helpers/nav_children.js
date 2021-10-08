c686ba0eb45f925c80958e6c02a84467c1c32a10
Rob Winch <rwinch@users.noreply.github.com>
2021-10-08 17:12:47 -0500

    Enable Navigation summary with page-nav-section-summary attribute

---
+++ src/helpers/nav_children.js
@@ -0,0 +1,21 @@
+'use strict'
+
+module.exports = (navigation, url) => {
+  // console.log(url);
+  return findNavigation(navigation[0].items, url);
+}
+
+function findNavigation(items, url) {
+  if (!items) {
+    return;
+  }
+  for (const item of items) {
+    const r = findNavigation(item.items, url);
+    if (r) {
+      return r;
+    }
+    if (item.url == url) {
+      return item.items;
+    }
+  }
+}
