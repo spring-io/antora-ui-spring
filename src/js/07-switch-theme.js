175db236eed3fd148d12414325f5e8da14534778
Dan Allen <dan@opendevise.com>
2022-12-11 15:18:31 -0700

    fix flash of light theme on load

---
+++ src/js/07-switch-theme.js
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+;(function () {
+  'use strict'
+
+  activateSwitch(document.getElementById('switch-theme-checkbox'))
+
+  function activateSwitch (control) {
+    if (!control) return
+    control.checked = document.documentElement.classList.contains('dark-theme')
+    control.addEventListener('change', onThemeChange.bind(control))
+  }
+
+  function onThemeChange () {
+    document.documentElement.classList.toggle('dark-theme', this.checked)
+    saveTheme(this.checked ? 'dark' : 'light')
+  }
+
+  function saveTheme (theme) {
+    window.localStorage && window.localStorage.setItem('theme', theme)
+  }
+})()
