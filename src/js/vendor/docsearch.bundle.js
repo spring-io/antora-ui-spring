9fbc5c950aa394221389f6b356aa4b946df944f4
Damien Vitrac <dvitrac@vmware.com>
2023-04-26 17:50:09 +0200

    Instand Search

--- src/js/vendor/docsearch.bundle.js
+++ src/js/vendor/docsearch.bundle.js
@@ -1,15 +1,115 @@
-;(function () { /*! docsearch 2.6.x | © Algolia | github.com/algolia/docsearch */
+/* eslint-disable no-undef */
+
+;(function () {
   'use strict'
+  var MicroModal = require('micromodal')
+  const config = document.getElementById('search-script').dataset
+  const client = algoliasearch(config.appId, config.apiKey)
 
-  activateSearch(require('@docsearch/js'), document.getElementById('search-script').dataset)
+  const search = instantsearch({
+    indexName: config.indexName,
+    searchClient: client,
+  })
 
-  function activateSearch (docsearch, config) {
-    var searchField = document.getElementById(config.searchFieldId || 'search')
-    docsearch({
-      container: searchField,
-      appId: config.appId,
-      indexName: config.indexName,
-      apiKey: config.apiKey,
+  let lastRenderArgs
+
+  const transformItems = (items) => {
+    return items.map((item) => {
+      const label = Object.keys(item.hierarchy).reduce((acc, key) => {
+        if (item._highlightResult.hierarchy[key]) {
+          return item._highlightResult.hierarchy[key]
+        }
+        return acc
+      }, '')
+      return {
+        ...item,
+        _highlightResult: {
+          ...item._highlightResult,
+          label: label,
+        },
+      }
     })
   }
+
+  const infiniteHits = instantsearch.connectors.connectInfiniteHits(
+    (renderArgs, isFirstRender) => {
+      const { hits, showMore, widgetParams } = renderArgs
+      const { container } = widgetParams
+      lastRenderArgs = renderArgs
+      if (isFirstRender) {
+        const sentinel = document.createElement('div')
+        container.appendChild(document.createElement('ul'))
+        container.appendChild(sentinel)
+        const observer = new IntersectionObserver((entries) => {
+          entries.forEach((entry) => {
+            if (entry.isIntersecting && !lastRenderArgs.isLastPage) {
+              showMore()
+            }
+          })
+        })
+        observer.observe(sentinel)
+        return
+      }
+      const _hits = transformItems(hits)
+      container.querySelector('ul').innerHTML = _hits
+        .map((hit) => {
+          let content = ''
+          let breadcrumbs = ''
+
+          if (hit.content) {
+            content = `<p class="hit-description">
+              ${instantsearch.snippet({ hit: hit, attribute: 'content' })}
+            </p>`
+          }
+
+          if (hit.breadcrumbs) {
+            breadcrumbs = `<div class="hit-breadcrumbs">
+              ${hit.breadcrumbs.map((chain) => {
+                const arr = chain.split('|')
+                return `<span>${arr[0]}</span>`
+              }).join(' > ')}
+            </div>`
+          }
+
+          return `<li>
+            <a href="${hit.url}" class="ais-Hits-item">
+              <div class="hit-name">
+                ${instantsearch.highlight({ hit: hit, attribute: 'label' })}
+              </div>
+              ${breadcrumbs}
+              ${content}
+            </a>
+          </li>`
+        })
+        .join('')
+    }
+  )
+
+  search.addWidgets([
+    instantsearch.widgets.configure({
+      facetFilters: [`version:${config.pageVersion}`],
+      distinct: true,
+    }),
+    instantsearch.widgets.searchBox({
+      container: '#searchbox',
+      autofocus: true,
+      showSubmit: false,
+      showReset: false,
+      placeholder: 'Search in the current documentation',
+    }),
+    infiniteHits({
+      container: document.querySelector('#hits'),
+    }),
+  ])
+
+  search.start()
+
+  MicroModal.init()
+
+  document.getElementById('search').addEventListener('click', () => {
+    MicroModal.show('modal-1', {
+      // onClose: (modal) => console.info(`${modal.id} is hidden`),
+      disableScroll: true,
+    })
+  })
 })()
