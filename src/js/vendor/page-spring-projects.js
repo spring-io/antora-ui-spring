7cc7e2a51dbe91379c843b4a695b99993c5d86b3
Damien Vitrac <dvitrac@vmware.com>
2023-10-09 18:22:14 +0200

    Spring projects UI

---
+++ src/js/vendor/page-spring-projects.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+
+(function () {
+  document.querySelectorAll('.anchor').forEach((el) => {
+    const list = el.parentElement.querySelector('ul')
+    el.addEventListener('click', function (e) {
+      el.classList.toggle('active')
+      list.classList.toggle('show')
+    })
+  })
+})()
