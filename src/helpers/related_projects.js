6e9727988b673b4c072afb6df4ec61c9f32e7973
Chris Bono <chris.bono@gmail.com>
2023-10-12 00:21:51 -0500

    Update related project text for Spring Pulsar
    The current text is 'Spring Pulsar' but instead needs to be 'Spring for Apache Pulsar'.

--- src/helpers/related_projects.js
+++ src/helpers/related_projects.js
@@ -237,7 +237,7 @@ function relatedProjects (categories = [], projectIds = []) {
     { href: 'https://docs.spring.io/spring-graphql/reference/', id: 'graphql', text: 'Spring GraphQL' },
     { href: 'https://docs.spring.io/spring-kafka/reference/', id: 'kafka', text: 'Spring for Apache Kafka' },
     { href: 'https://docs.spring.io/spring-modulith/reference/', id: 'modulith', text: 'Spring Modulith' },
-    { href: 'https://docs.spring.io/spring-pulsar/reference/', id: 'pulsar', text: 'Spring Pulsar' },
+    { href: 'https://docs.spring.io/spring-pulsar/reference/', id: 'pulsar', text: 'Spring for Apache Pulsar' },
     { href: 'https://docs.spring.io/spring-shell/reference/', id: 'shell', text: 'Spring Shell' },
   ]
   return projectDocumentation.filter(
