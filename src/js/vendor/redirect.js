2a282d3c2704f92b7796d7df67ba74f769bb9be4
Phillip Webb <phil.webb@broadcom.com>
2024-03-26 09:19:45 -0700

    Add redirect support
    Add a new 'redirect' layout that can be used to as a target of
    an Apache HTTPD rewrite rule in order to perform redirects based
    on a page or anchor.
    
    Redirect pages can be written in asciidoc in the following form:
    
    	:page-layout: redirect
    
    	* xref:<new-location>[<previous-page><#previous-anchor>]
    	...
    
    Closes gh-221

---
+++ src/js/vendor/redirect.js
@@ -0,0 +1,24 @@
+;(function () {
+  'use strict'
+
+  window.addEventListener('load', onChange)
+  window.addEventListener('hashchange', onChange)
+
+  function onChange () {
+    const params = new URLSearchParams(window.location.search)
+    const page = params.get('page') || ''
+    const fragment = window.location.hash
+    const pageAndFragment = page + ((fragment.length === 1) ? '' : fragment)
+    let foundForFragment
+    let foundForPageAndFragment
+    const candidates = document.querySelector('body ul')
+    if (candidates) {
+      for (const candidate of candidates.children) {
+        const anchorElement = candidate.querySelector('a')
+        if (anchorElement.text === pageAndFragment) foundForPageAndFragment = anchorElement.href
+        if (anchorElement.text === fragment) foundForFragment = anchorElement.href
+      }
+    }
+    window.location.replace(foundForPageAndFragment || foundForFragment || 'index.html')
+  }
+})()
