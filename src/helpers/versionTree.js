d482b211fb87f4428574e18144fa3dd5fe4cf7b2
Phillip Webb <phil.webb@broadcom.com>
2024-06-17 09:21:38 -0700

    Fix version component tree

--- src/helpers/versionTree.js
+++ src/helpers/versionTree.js
@@ -1,25 +1,21 @@
 'use strict'
 
+module.exports = (components) => versionTree(components)
+
+function versionTree (components) {
+  for (const [, component] of Object.entries(components)) {
+    component.versionTree = splitVersions(component.versions)
+  }
+  return components
+}
+
 function splitVersions (versions) {
-  const snapshot = versions.filter((v) => v.version.includes('SNAPSHOT'))
-  const stable = versions.filter((v) => {
-    const split = v.version.split('-')
-    if (split.length === 1) return true
-    return false
-  })
+  const snapshot = versions.filter((v) => v.displayVersion.includes('SNAPSHOT'))
+  const stable = versions.filter((v) => !v.displayVersion.includes('-'))
   const preview = versions.filter((v) => !snapshot.includes(v) && !stable.includes(v))
   return {
     snapshot: snapshot.length > 0 ? snapshot : null,
     stable: stable.length > 0 ? stable : null,
     preview: preview.length > 0 ? preview : null,
   }
-}
-
-module.exports = (components) => {
-  return Object.values(components)?.map((comp) => {
-    return {
-      ...comp,
-      versions: splitVersions(comp.versions),
-    }
-  })
 }
