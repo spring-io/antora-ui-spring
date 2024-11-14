24b0ea9bb868390e1ef3290e476fd66788c083d5
Rob Winch <362503+rwinch@users.noreply.github.com>
2024-11-14 08:37:11 -0600

    feat: Stay on the same page when switching version
    Closes gh-269

--- test/versionTree-test.js
+++ test/versionTree-test.js
@@ -5,7 +5,7 @@ const { expect } = require('./harness')
 const versionTree = require('../src/helpers/versionTree.js')
 
 describe('versionTree', () => {
-  it('should return stable, preview and snapshot  versions', () => {
+  it('should return stable, preview and snapshot versions', () => {
     const result = versionTree({
       test: {
         versions: [
@@ -43,6 +43,93 @@ describe('versionTree', () => {
     expect(tree.snapshot.length).is.eql(2)
     expect(tree.snapshot[0].displayVersion).is.eql('3.0.1-SNAPSHOT')
     expect(tree.snapshot[1].displayVersion).is.eql('3.0.0-SNAPSHOT')
+  })
+
+  it('page version overrides urls when component names are the same', () => {
+    const result = versionTree({
+      test: {
+        name: 'test',
+        versions: [
+          {
+            displayVersion: '2.0.0',
+            url: './version.html',
+          },
+        ],
+      },
+    }, {
+      component: {
+        name: 'test',
+      },
+      versions: [
+        {
+          displayVersion: '2.0.0',
+          url: './page.html',
+        },
+      ],
+    })
+
+    const tree = result.test.versionTree
+    expect(tree.stable.length).is.eql(1)
+    expect(tree.stable[0].displayVersion).is.eql('2.0.0')
+    expect(tree.stable[0].url).is.eql('./page.html')
+  })
+
+  it('does not override if page does not define same version', () => {
+    const result = versionTree({
+      test: {
+        name: 'test',
+        versions: [
+          {
+            displayVersion: '2.0.0',
+            url: './version.html',
+          },
+        ],
+      },
+    }, {
+      component: {
+        name: 'test',
+      },
+      versions: [
+        {
+          displayVersion: '1.0.0',
+          url: './page.html',
+        },
+      ],
+    })
+
+    const tree = result.test.versionTree
+    expect(tree.stable.length).is.eql(1)
+    expect(tree.stable[0].displayVersion).is.eql('2.0.0')
+    expect(tree.stable[0].url).is.eql('./version.html')
+  })
+
+  it('page versions do not override if different component name', () => {
+    const result = versionTree({
+      test: {
+        name: 'test',
+        versions: [
+          {
+            displayVersion: '2.0.0',
+            url: './version.html',
+          },
+        ],
+      },
+    }, {
+      component: {
+        name: 'baz',
+      },
+      versions: [
+        {
+          displayVersion: '2.0.0',
+          url: './page.html',
+        },
+      ],
+    })
+
+    const tree = result.test.versionTree
+    expect(tree.stable.length).is.eql(1)
+    expect(tree.stable[0].displayVersion).is.eql('2.0.0')
+    expect(tree.stable[0].url).is.eql('./version.html')
   })
 
   it('should return an empty structure', () => {
